68c7042dbf9c82ede2b3ee1ab29eef037221011f
memcmahon <mcmahon.meganelizabeth@gmail.com>
2025-04-01 16:17:07 -0600

    add player

---
+++ spec/player_spec.rb
@@ -0,0 +1,46 @@
+require 'rspec'
+require './lib/player'
+require './lib/deck'
+require './lib/card'
+
+RSpec.describe Player do
+  it "exists" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+
+    expect(player).to be_an_instance_of(Player)
+  end
+
+  it "has readable attributes" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+
+    expect(player.name).to eq('Clarisa')
+    expect(player.deck).to eq(deck)
+  end
+
+  it "can check if the player has lost" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+
+    expect(player.has_lost?).to eq(false)
+
+    player.deck.remove_card
+    player.deck.remove_card
+    player.deck.remove_card
+
+    expect(player.has_lost?).to eq(true)
+  end
+end
